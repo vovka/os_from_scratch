15592e1d8f769d96c28139210d53212abaebc884
Volodymyr Shcherbyna <sch.icq@gmail.com>
2015-11-15 12:38:00 +0200

    stack

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,33 +1,29 @@
 ;
-; A simple boot sector program that demonstrates addressing.
+; A simple boot sector program that demonstrates the stack.
 ;
 
-[org 0x7c00]
-
 mov ah, 0x0e            ; int 10/ ah = 0eh -> scrolling teletype BIOS routine
+mov bp, 0x8000          ; Set the base of the stack a little above where BIOS
+mov sp, bp              ; loads our boot sector - so it won't overwrite us.
 
-; ; First attempt
-; mov al, the_secret
-; int 0x10              ; Does this print an X?
+push 'A'                ; Push some characters on the stack for later
+push 'B'                ; retreival. Note, these are pushed on as
+push 'C'                ; 16 - bit values, so the most significant byte
+                        ; will be added by our assembler as 0x00.
 
-; ; Second attempt
-; mov al, [the_secret]
-; int 0x10              ; Does this print an X?
+pop bx                  ; Note, we can only pop 16 - bits, so pop to bx
+mov al, bl              ; then copy bl ( i.e. 8- bit char ) to al
+int 0x10                ; print (al)
 
-; Third attempt
-mov bx, the_secret
-; add bx, 0x7c00
-mov al, [bx]
-int 0x10                ; Does this print an X?
+pop bx                  ; Pop the next value
+mov al, bl
+int 0x10                ; print (al)
 
-; Fourth attempt
-mov al, [0x7c10]
-int 0x10              ; Does this print an X?
+mov al, [0x7ffe]        ; To prove our stack grows downwards from bp ,
+                        ; fetch the char at 0x8000 - 0x2 ( i.e. 16 - bits )
+int 0x10                ; print (al)
 
-jmp $                 ; Jump forever.
-
-the_secret :
-  db "X"
+jmp $                   ; Jump forever.
 
 ; Padding and magic BIOS number.
 times 510-($-$$) db 0
